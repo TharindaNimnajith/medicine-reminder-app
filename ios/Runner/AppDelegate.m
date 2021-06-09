14abe1dbac76bf2cd9e6ee943a42db3429fcc861
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-06-09 21:22:32 +0530

    test

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,18 @@
+#import "AppDelegate.h"
+#import "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
